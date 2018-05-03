af71d4ac8e5b71c563d426e459144b4125ea98b7
malinicz <malinicz@gmail.com>
2018-05-03 14:18:01 +0200

    add menu items and animations for navigation

--- components/Navigation.js
+++ components/Navigation.js
@@ -1,8 +1,18 @@
 import React from 'react';
 import { bool } from 'prop-types';
-import styled from '~/styles';
+import styled, { keyframes } from '~/styles';
+
+import navigationConfig from '~/constants/navigationConfig';
+
+const violinFlash = keyframes`
+  0% { opacity: 0.02;}
+  50% {opacity: 0.06;}
+  100% {opacity: 0.02;}
+`;
 
 const NavigationHolder = styled.nav`
+  display: flex;
+  justify-content: center;
   position: fixed;
   top: 0;
   right: 0;
@@ -19,29 +29,96 @@ const Block = styled.div`
   left: 0;
   height: ${props => (props.isVisible ? props.height : '0%')};
   background-color: ${props => props.theme.colors.lightDark};
-  opacity: ${props => (props.isVisible ? 0.95 : 0)};
+  opacity: ${props => (props.isVisible ? 0.98 : 0)};
   transition: 0.5s ease height, 0.4s ease opacity;
   transition-delay: ${props => props.delay}s;
+`;
+
+const ViolinImage = styled.img`
+  position: relative;
+  z-index: 11;
+  height: 100%;
+  animation: ${props =>
+    props.isVisible ? `${violinFlash} 10s ease infinite` : 'none'};
+  visibility: ${props => (props.isVisible ? 'visible' : 'hidden')};
+`;
+
+const MenuItemsHolder = styled.ul`
+  display: flex;
+  position: absolute;
+  z-index: 12;
+  top: 45%;
+  list-style-type: none;
+  opacity: ${props => (props.isVisible ? 1 : 0)};
+  transition: 0.3s ease opacity;
+`;
+
+const MenuItem = styled.li`
+  display: flex;
+  align-items: center;
+
+  &:first-child {
+    &::before {
+      content: '■';
+      font-size: 1.5em;
+      margin: 0 12px;
+    }
+  }
+
+  &::after {
+    content: '■';
+    font-size: 1.5em;
+    margin: 0 12px;
+  }
+`;
+
+const LinkElement = styled.a`
+  position: relative;
+  font-family: 'MitrRegular';
+  font-size: 2em;
+  text-transform: uppercase;
+  transition: 0.3s color ease;
+  line-height: 1.2em;
+  cursor: pointer;
+
+  &::before {
+    content: '';
+    position: absolute;
+    z-index: -1;
+    top: 95%;
+    right: 50%;
+    bottom: -6%;
+    left: 50%;
+    background-color: ${props => props.theme.colors.darkDark};
+    transition: 0.3s right ease, 0.3s left ease;
+  }
+
+  &:hover {
+    &::before {
+      right: 0;
+      left: 0;
+    }
+  }
 `;
 
 export const Navigation = ({ isVisible }) => {
   return (
     <NavigationHolder isVisible={isVisible}>
+      <ViolinImage
+        src="/static/images/violin-black.svg"
+        isVisible={isVisible}
+      />
       <Block top={0} height="40%" isVisible={isVisible} />
       <Block top="40%" height="30%" isVisible={isVisible} />
       <Block top="70%" height="20%" isVisible={isVisible} />
       <Block top="90%" height="10%" isVisible={isVisible} />
-      <ul>
-        <li>
-          <a href="#">menu item</a>
-        </li>
-        <li>
-          <a href="#">menu item 2</a>
-        </li>
-        <li>
-          <a href="#">menu item 3</a>
-        </li>
-      </ul>
+      <MenuItemsHolder isVisible={isVisible}>
+        {navigationConfig.map(navItem => (
+          <MenuItem key={navItem.value}>
+            <LinkElement to={navItem.value}>{navItem.label}</LinkElement>
+          </MenuItem>
+        ))}
+      </MenuItemsHolder>
     </NavigationHolder>
   );
 };
---
+++ constants/navigationConfig.js
@@ -0,0 +1,15 @@
+export default [
+  { label: 'o kwartecie', value: 'o-kwartecie' },
+  { label: 'oferta', value: 'oferta' },
+  { label: 'współpraca', value: 'wspolpraca' },
+  { label: 'multimedia', value: 'multimedia' },
+  { label: 'kontakt', value: 'kontakt' },
+];
+
+export const navigationEnglish = [
+  { label: 'about us', value: 'about-us' },
+  { label: 'offer', value: 'offer' },
+  { label: 'cooperation', value: 'cooperation' },
+  { label: 'multimedia', value: 'multimedia' },
+  { label: 'contact', value: 'contact' },
+];
